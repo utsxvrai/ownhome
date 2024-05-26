ddaa5f2179c22043bda4e5e728e33e0cf56a6b66
Utsav Rai <utsavrai2115@gmail.com>
2024-05-26 12:06:38 +0530

    created the header of the ownHone

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signoff from "./pages/Signoff";
 import Forgotpassword from './pages/Forgotpassword';
-
+import Header from "./components/Header";
+import Offers from "./pages/Offers";
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-off" element={<Signoff />} />
           <Route path="/forgotpassword" element={<Forgotpassword />} />
+          <Route path="offers" element={<Offers />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useLocation , useNavigate } from 'react-router-dom'
+
+export default function Header() {
+    const location = useLocation();
+    function pathMatchRoute(route){
+        if(route === location.pathname){
+            return true;
+        }
+    }
+    const navigate = useNavigate(); 
+
+    return (
+    <div className='bg-green-800 border-b shadow-sm sticky' >
+        <header  className='flex justify-between items-center px-3 max-w-6xl mx-auto' >
+            <div >
+                <img src='/ownLogo.png' alt='logo' className='h-12' cursor-pointer
+                onClick ={()=>navigate('/')} />    
+            </div>
+            <div>
+                <ul className='flex space-x-10 '>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/") && "text-white border-b-yellow-400"}` 
+                    }
+                    onClick ={()=>navigate('/')}
+                    
+                    >Home</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-white border-b-yellow-400"}`}
+                
+                onClick={() => navigate("offers")}
+                >Offers</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/sign-in") && "text-white border-b-yellow-400"}` }
+                onClick={()=>navigate('sign-in')}
+                >Sign In</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+    )
+}
